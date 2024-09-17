754c863e60e34c2f9c1c55ec411224f797bc5a3c
koneko <67551503+koneko@users.noreply.github.com>
2024-09-17 23:46:44 +0200

    "witches do not lie"

--- src/classes/Grid.ts
+++ src/classes/Grid.ts
@@ -1,9 +1,29 @@
 import * as PIXI from "pixi.js";
+import { Tower } from "./Tower.ts";
+
+export enum CellType {
+  Path,
+  NoBuild,
+  Build,
+}
+
+export class Cell {
+  public x: number;
+  public y: number;
+  public type: CellType;
+  public tower: Tower;
+  constructor(x, y, type) {
+    this.x = x;
+    this.y = y;
+    this.type = type;
+  }
+}
 
 export class Grid {
   public rows: number;
   public columns: number;
   public gridObject: PIXI.Container;
+  public cells: Array<Cell>;
   constructor(x, y, width, height, rows, columns) {
     this.rows = rows;
     this.columns = columns;
@@ -13,6 +33,9 @@ export class Grid {
       x: x,
       y: y,
     });
+    for (let index = 0; index < rows * columns; index++) {
+      // const cell = new Cell();
+    }
   }
 
   getPixelScalingFactor() {
@@ -24,6 +47,7 @@ export class Grid {
   gridUnitsToPixels(amount: number): number {
     return amount * this.getPixelScalingFactor();
   }
+
   pixelsToGridUnits(pixels: number): number {
     return pixels / this.getPixelScalingFactor();
   }

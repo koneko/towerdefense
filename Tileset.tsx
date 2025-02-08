37407c1234515fd8d6701d011f6aa072dd293340
koneko <koneko@users.noreply.github.com>
2025-02-08 17:29:40 +0000

    deploy: 1f62c8fdf0872e3f01af12f8348390e253371641

--- Tileset.tsx
+++ Tileset.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.11.0" name="TiledTDThree64" tilewidth="64" tileheight="64" tilecount="140" columns="14">
- <image source="../../../../dumping/tiles/TiledTDThree64.png" width="896" height="640"/>
+ <image source="./TiledTDThree64.png" width="896" height="640"/>
 </tileset>

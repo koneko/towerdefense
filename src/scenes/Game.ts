a3e8be3c187d8eacf7fc39c3e3db92a6f28c2bbb
koneko <67551503+koneko@users.noreply.github.com>
2025-03-07 01:08:17 +0100

    balanced gems

--- src/scenes/Game.ts
+++ src/scenes/Game.ts
@@ -123,7 +123,7 @@ export class GameScene extends Scene {
             this.changeRoundButton.buttonIcon.texture = GameAssets.FastForwardIconTexture;
             this.events.emit(WaveManagerEvents.NewWave, `${this.currentRound + 1}`);
         };
-        this.MissionStats = new MissionStats(100, 250);
+        this.MissionStats = new MissionStats(100, 75);
         this.events.on(GemEvents.TowerPanelSelectGem, (gem, index, tower) => {
             if (gem == null) {
                 if (!this.MissionStats.checkIfPlayerHasAnyGems())
